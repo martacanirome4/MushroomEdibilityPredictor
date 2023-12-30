5b0e8f94a298a1afa379aed9b80622fcd6c7e5d2
MartaCanino <martacaninoromero@gmail.com>
2023-12-30 12:12:22 +0000

    Itera por todos los valores de test y devuelve string con predicciones (5)

--- src/modelos/Modelo.java
+++ src/modelos/Modelo.java
@@ -62,8 +62,24 @@ public class Modelo {
             String[] valoresAtributos = {"e", "p"};
 
             Classifier clasificador  = (Classifier) weka.core.SerializationHelper.read("./models/J58.model");
+
             Instances data = leerInstancias("./test_data/test.arff");
-            return valoresAtributos[(int) clasificador.classifyInstance(data.instance(0))];
+
+            // Build string form results to store predictions for all instances
+            StringBuilder res = new StringBuilder();
+
+            for (int i = 0; i < data.numInstances(); i++) {
+                res.append(valoresAtributos[(int) clasificador.classifyInstance(data.instance(i))]);
+    
+                // Add a comma and space unless it's the last prediction
+                if (i < data.numInstances() - 1) {
+                    res.append(", ");
+                }
+            }
+    
+            return res.toString();
+    
+
         }catch (Exception ex) {
             Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
             return "Error al intentar leer el modelo";
